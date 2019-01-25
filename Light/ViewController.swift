40401173b26304f0fad0f0f2a3bb478fd0278e06
Andrew Ponomarenko <soadap@soadaps-macbook-pro.local>
2019-01-24 18:12:37 -0600

    initial and final commit

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var switchState = true
+    @IBOutlet weak var switchOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func switchChanged(_ sender: Any) {
+        switchState = !switchState
+        updateUI()
+    }
+    
+    func updateUI() {
+        if switchState {
+            view.backgroundColor = UIColor.white
+            switchOutlet.setTitle("🌚", for: .normal)
+        } else {
+            view.backgroundColor = UIColor.black
+            switchOutlet.setTitle("🌝", for: .normal)
+        }
+    }
+    
 }
 
